2e1ac2156f244c049921e23943e543dcb41a8830
dm1tro69 <dimolg22@gmail.com>
2020-04-06 00:19:17 +0300

    add serializers

---
+++ news/api/serializers.py
@@ -0,0 +1,41 @@
+from rest_framework import serializers
+from news.models import Article
+
+class ArticleSerializer(serializers.Serializer):
+    id = serializers.IntegerField(read_only=True)
+    author = serializers.CharField()
+    title = serializers.CharField()
+    description = serializers.CharField()
+    body = serializers.CharField()
+    location = serializers.CharField()
+    publication_date = serializers.DateField()
+    active = serializers.BooleanField()
+    created_at = serializers.DateTimeField(read_only=True)
+    updated_at = serializers.DateTimeField(read_only=True)
+
+    def create(self, validated_data):
+        print(validated_data)
+        return Article.objects.create(**validated_data)
+
+    def update(self, instance, validated_data):
+        instance.author = validated_data.get('author', instance.author)
+        instance.title = validated_data.get('title', instance.title)
+        instance.description = validated_data.get('description', instance.description)
+        instance.body = validated_data.get('body', instance.body)
+        instance.location = validated_data.get('location', instance.location)
+        instance.publication_date = validated_data.get('publication_date', instance.publication_date)
+        instance.active = validated_data.get('active', instance.active)
+        instance.save()
+        return instance
+
+    def validate(self, data):
+        """check that description and title are different"""
+        if data['title'] == data['description']:
+            raise serializers.ValidationError('Title and description must be different from one another!')
+        return data
+
+
+    def validate_title(self, value):
+        if len(value) < 50:
+            raise serializers.ValidationError('The title has to be at least 50 chars long!')
+        return value
