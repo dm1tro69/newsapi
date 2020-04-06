5c53c8431e4d0bdea38c7cc72aa37554a81c4530
dm1tro69 <dimolg22@gmail.com>
2020-04-06 17:43:16 +0300

    add last commit

--- news/api/urls.py
+++ news/api/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path
 #from .views import article_list_create_api_view, article_detail_api_view
-from .views import ArticleDetailAPIView, ArticleListCreateAPIView
+from .views import ArticleDetailAPIView, ArticleListCreateAPIView, JournalistListCreateAPIView
 
 urlpatterns = [
      path('articles/', ArticleListCreateAPIView.as_view(), name='article-list'),
      path('articles/<int:pk>/', ArticleDetailAPIView.as_view(), name='article-detail'),
+     path('journalists/', JournalistListCreateAPIView.as_view(), name='journalists-list'),
 
     # path('articles/', article_list_create_api_view, name='article-list'),
     # path('articles/<int:pk>/', article_detail_api_view, name='article-detail'),
